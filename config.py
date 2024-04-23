5ed2cea61fa44e3534a598ff86dd7b6ff46d6298
hajun <dear20120127@gmail.com>
2024-04-23 22:18:24 +0800

    5th commit

---
+++ config.py
@@ -0,0 +1,6 @@
+# imgur key
+client_id = '339c1d77aa62b9b'
+client_secret = 'a5e7fa5475f11a74566bc0f9b06753b549e9a194'
+album_id = 'HB9JiLH'
+access_token = '5d17154aa24658538a7f2f6ce8c003ea5e5aaf02'
+refresh_token = '16a41f35a0c11778d224fcecfeae7b3f98bb225c'
